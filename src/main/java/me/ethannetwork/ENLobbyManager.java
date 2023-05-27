3b1c02c890601a486882cbf8ae5c76bdf2c98afd
EthanGarey <ethgarey@gmail.com>
2023-05-26 21:24:00 -0400

    PushV1

---
+++ src/main/java/me/ethannetwork/ENLobbyManager.java
@@ -0,0 +1,17 @@
+package me.ethannetwork;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ENLobbyManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
